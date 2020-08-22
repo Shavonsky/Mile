4d90541074dc212d59e7a191c6fb03603acb7ac7
Ekaterina Popova <katerina.shavonsky@yandex.ru>
2020-08-22 21:25:39 +0300

    Java_mile

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+       int price = 50000;
+       int rub = 20;
+       int mile = price / rub;
+
+        System.out.println(mile);
+    }
+}
+
